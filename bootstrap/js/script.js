82eba15f7897e54c2f91479df2e5204ae621c7f7
Alex <46680379+BlodAlex2@users.noreply.github.com>
2021-06-09 21:03:43 +0300

    add new sections

---
+++ bootstrap/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function testWebP(callback) {
+  var webP = new Image();
+
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+} //фукция для проверки поддерживает ли браузер webp
+
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+}); // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
